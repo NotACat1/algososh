4569001f30bca890b1a5866d0633496cc9f4c7ed
NotACat1 <notacat.mail@gmail.com>
2024-04-14 14:58:58 +0300

    feat: добавление компонентов

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -26,7 +26,7 @@ export const Button: React.FC<ButtonProps> = ({
 }) => {
   const currentIcon =
     sorting === Direction.Ascending ? <AscendingIcon /> : <DescendingIcon />;
-  const className = `text text_type_button text_color_primary ${
+  const className = `text_type_button text_color_primary ${
     styles.button
   } ${linkedList && styles[linkedList]} ${
     isLoader && styles.loader
