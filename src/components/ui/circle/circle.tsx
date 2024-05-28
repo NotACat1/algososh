2b10412dd37a3bc44e44ee22a57208c3a9d229e0
NotACat1 <notacat.mail@gmail.com>
2024-05-28 20:15:15 +0300

    feat: добаление тестов для компонента `Circle`

--- src/components/ui/circle/circle.tsx
+++ src/components/ui/circle/circle.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import styles from "./circle.module.css";
-import { ElementStates } from "../../../types/element-states";
+import React from 'react';
+import styles from './circle.module.css';
+import { ElementStates } from '../../../types/element-states';
 
 interface CircleProps {
   state?: ElementStates;
@@ -8,7 +8,7 @@ interface CircleProps {
   head?: string | React.ReactElement | null;
   index?: number;
   tail?: string | React.ReactElement | null;
-  tailType?: "string" | "element";
+  tailType?: 'string' | 'element';
   extraClass?: string;
   isSmall?: boolean;
 }
@@ -19,7 +19,7 @@ export const Circle: React.FC<CircleProps> = ({
   head,
   index,
   tail,
-  extraClass = "",
+  extraClass = '',
   isSmall,
 }) => {
   return (
@@ -28,13 +28,13 @@ export const Circle: React.FC<CircleProps> = ({
         className={`text text_type_input text_color_input mb-4 ${
           styles.absolute
         } ${styles.head} ${
-          styles[typeof head === "string" ? "string" : "element"]
+          styles[typeof head === 'string' ? 'string' : 'element']
         }`}
       >
         {head}
       </div>
       <div
-        className={`${styles.circle}  ${isSmall ? styles.small : ""} ${
+        className={`${styles.circle}  ${isSmall ? styles.small : ''} ${
           styles[state]
         }`}
       >
@@ -53,7 +53,7 @@ export const Circle: React.FC<CircleProps> = ({
         className={`text text_type_input text_color_input mt-4 ${
           styles.absolute
         } ${index?.toString() ? styles.tail60 : styles.tail30} ${
-          styles[typeof tail === "string" ? "string" : "element"]
+          styles[typeof tail === 'string' ? 'string' : 'element']
         }`}
       >
         {tail}
